1da208c6c07d2ecb82ca6af105cec56bae3046d9
Lucian Boatright <lucian.boatright@gmail.com>
2020-01-09 16:18:05 +0000

    add damage method

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,17 @@
 class Player
-  attr_reader :name
+  attr_reader :name, :hp
+
+  DEFAULT_HP = 100
+
+  DAMAGE_HP = 10
   
-  def initialize(name)
+  def initialize(name, hp = DEFAULT_HP)
     @name = name
+    @hp = hp
+  end
+
+  def attack
+    @hp -= DAMAGE_HP
   end
 
 end
