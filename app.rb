a60f3842f4dd453f825adfc2fcf6f69dd45a1538
shaunlsy <shaunlowshuennyuh@hotmail.com>
2020-01-09 15:21:59 +0000

    check player's name in player.rb

--- app.rb
+++ app.rb
@@ -1,30 +1,29 @@
 require 'sinatra/base'
+require 'player'
 
 class Battle < Sinatra::Base
-  enable :sessions
+  enable :Player
 
   get '/' do 
     erb :index
   end
 
-
-
   post '/names' do
-    session[:player_1] = params[:player_1]
-    session[:player_2] = params[:player_2]
+    $player_1 = Player.new(params[:player_1])
+    $player_2 = Player.new(params[:player_2])
     # erb :play
     redirect '/play'
   end
 
   get '/play' do
-    @player_1 = session[:player_1]
-    @player_2 = session[:player_2]
+    @player_1 = $player_1.name
+    @player_2 = $player_2.name
     erb :play
   end
 
   get '/attack' do
-    @player_1 = session[:player_1]
-    @player_2 = session[:player_2]
+    @player_1 = $player_1.name
+    @player_2 = $player_2.name
     erb :attack
   end
 
---
+++ spec/player_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require 'player'
+
+describe Player do
+  subject(:player_1){ described_class.new("Player_1") }
+  
+  it 'returns the player name' do
+    expect(subject.name).to eq "Player_1"
+  end
+end
